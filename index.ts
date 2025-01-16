89973ac9761e28be494ab263d2d64dcca6f2e843
Jason Buckner <github@jasonbuckner.com>
2025-01-16 14:56:46 -0800

    Add Exports (#9)
    * Add exports
    
    * Bump version

--- index.ts
+++ index.ts
@@ -1,5 +1,9 @@
 export { MetadataResponse } from './src/responses/metadata-response';
 
+export { File } from './src/models/file';
+export { Review } from './src/models/review';
+export { SpeechMusicASREntry } from './src/models/speech-music-asr-entry';
+
 export { DefaultMetadataBackend } from './src/backend/default-metadata-backend';
 export { MetadataService } from './src/metadata-service';
 export { MetadataServiceError } from './src/metadata-service-error';
